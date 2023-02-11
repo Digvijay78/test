21c9723e2a9a1a3ba9e7fe77301295ce81a0cafd
Digvijay78 <singhdigvijay1605@gmail.com>
2023-02-11 14:05:04 +0530

    jhk

--- src/App.js
+++ src/App.js
@@ -1,101 +1,77 @@
-import "./App.css";
-import React from "react";
-
+import './App.css';
+import React from 'react';
 
 function App() {
-  const [formData, setFormData] = React.useState({
-    firstName: "",
-    Phone: "",
-    email: "",
-    Address: "",
-  });
+  const [formData, setFormData] = React.useState(
+    { firstName: "", Phone: "", email: "", Address: "" }
+  )
 
-  const [show, setShow] = React.useState(false);
+    const [show , setShow] = React.useState(false);
 
-  console.log(formData);
+  console.log(formData)
 
   function handleChange(event) {
-    setFormData((prevFormData) => {
+    setFormData(prevFormData => {
       return {
         ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
+        [event.target.name]: event.target.value
+      }
+    })
   }
+
+
 
   function prompt() {
     console.log(2);
   }
 
   function members() {
-    setShow((item) => !item);
+    setShow(item => !item);
   }
 
   return (
-    <div className="main-container">
-      <h2>Fitness club registration</h2>
+    <div className='form-style'>
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={handleChange}
+          name="firstName"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+          name="email"
+        />
+        <input
+          type="number"
+          placeholder="Phone Number"
+          onChange={handleChange}
+          name="Phone"
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          onChange={handleChange}
+          name="Address"
+        />
+      </form>
+      <div className='btn'>
 
-      <div className="final">
-        <form className="form-container">
-          <div className="input-label first">
-            <input
-              className="form"
-              type="text"
-              placeholder="Name"
-              onChange={handleChange}
-              name="firstName"
-            />
-          </div>
-
-          <div className="input-label">
-            <input
-              className="form"
-              type="email"
-              placeholder="Email"
-              onChange={handleChange}
-              name="email"
-            />
-          </div>
-          <div className="input-label">
-            <input
-              className="form"
-              type="number"
-              placeholder="Phone Number"
-              onChange={handleChange}
-              name="Phone"
-            />
-          </div>
-          <div className="input-label">
-            <input
-              className="form"
-              type="text"
-              placeholder="Address"
-              onChange={handleChange}
-              name="Address"
-            />
-          </div>
-        </form>
-        <div className="out-button">
-          <button onClick={prompt} className="form button">
-            Add Member
-          </button>
-        </div>
+        <button onClick={prompt} >Add Member</button>
+        <button onClick={members} >View Member List</button>
       </div>
 
-      <div className="out-button">
-        <button onClick={members} className="form button">
-          View Member List
-        </button>
-      </div>
-
-      <div className={show ? "visible" : "hidden"}>
+      <div className={show ? "visible" : "hidden"}  >
         <p>{`Name:${formData.firstName}`}</p>
         <p>{`Email:${formData.email}`}</p>
         <p>{`Phone Number:${formData.Phone}`}</p>
         <p>{`Address:${formData.Address}`}</p>
       </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
